7f5d43d16fade67ed46c695622dfde1fb30bcc69
georgi-i <worksaev@gmail.com>
2021-09-02 22:56:09 +0300

    Update

--- invoice_generator/static/invoice_generator/app.js
+++ invoice_generator/static/invoice_generator/app.js
@@ -1,22 +1,23 @@
 
-//$(document).ready(function(){
-//
-//    const companyId = $("input.company_id[type=number]");
-//
-//    companyId.keyup(function(e) {
-//        e.preventDefault();
-//
-//        if (companyId.val().length === 9) {
-//            
-//            $.ajax({ 
-//                type : "GET", 
-//                url : "/", 
-//                data: {
-//                    'company_id': companyId.val()
-//                }
-//              });
-//        }            
-//    });
-//});
-//
+$(document).ready(function(){
+
+    const quantity_field = $("input.quantity[type=number]");
+    const unit_price = $("input.unit_price[type=number]");
+    const value = $("input.value[type=number]");
+
+    function calcValue(x, y) {
+        if (x.val().length != 0 && y.val().length != 0) {
+            
+            return x.val() * y.val()
+        } 
+      } 
+
+    quantity_field.keyup(function() {        
+        value.val(calcValue(quantity_field, unit_price))            
+    });
+    unit_price.keyup(function() {        
+        value.val(calcValue(quantity_field, unit_price))            
+    });
+});
+
 
